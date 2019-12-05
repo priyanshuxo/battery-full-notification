f5b990e7fe8f1d7eb58ad4ce2ccaeba7e291f43d
priyanshuxo <58258217+priyanshuxo@users.noreply.github.com>
2019-12-05 10:56:24 +0530

    Update batteryfull.sh

--- batteryfull.sh
+++ batteryfull.sh
@@ -4,7 +4,7 @@ do
     export DISPLAY=:0.0
     battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
     if on_ac_power; then
-        if [ "$battery_percent" -gt 95 ]; then
+        if [ "$battery_percent" -gt 75 ]; then
             notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
             paplay /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
         fi
